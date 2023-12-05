c85b2fad42e8d9c4cbb59fd7b615ff5bcc0d0366
Farsab Butt <farsabb@gmail.com>
2023-12-05 23:01:46 +0100

    feat(comment): add second comment

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -2,6 +2,7 @@ const { resolve } = require("node:path");
 
 const project = resolve(process.cwd(), "tsconfig.json");
 // Add a new comment
+// Add a new comment # 2
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
